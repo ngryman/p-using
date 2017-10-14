b25720db9b08728844c0434a4c900eb1e1a56e44
ngryman <ngryman@gmail.com>
2017-10-14 21:47:58 +0200

    :wrench: migrate to new rollup syntax

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,11 @@ import babel from 'rollup-plugin-babel'
 import cleanup from 'rollup-plugin-cleanup'
 
 export default {
-  format: 'umd',
-  entry: 'index.js',
-  dest: 'dist/p-using.js',
+  input: 'index.js',
+  output: {
+    file: 'dist/p-using.js',
+    format: 'umd'
+  },
   name: 'using',
   plugins: [
     resolve({ jsnext: true, main: true }),
